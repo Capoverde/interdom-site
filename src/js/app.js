110f94057fbdb48f0e2f7034eea5474b34e3d3d5
Piotr <piotr_as@yahoo.com>
2021-07-21 11:47:41 +0200

    rwd in contact - beginnig

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,11 @@
+/* styles */
 import '../scss/app.scss'
+
 /* libraries */
 import './aos'
 import './slider.js'
+import $ from "jquery";
+
 /* components  */
 import './nav.js'
 import './moreBtnHandler.js'
