cb47cc259fdc647b8f9909bf3f26b6496e88427b
Piotr <piotr_as@yahoo.com>
2021-08-04 15:09:18 +0200

    praca nad toglowanie przycisku more w ofertach

--- src/js/moreBtnHandler.js
+++ src/js/moreBtnHandler.js
@@ -1,11 +1,22 @@
 const moreBtn = document.querySelectorAll('.more--offer')
 const art = Array.from(document.querySelectorAll('.article--contentBox'))
-console.log(art)
+// console.log(art)
 const artList = Array.from(document.querySelectorAll('.article--contentBox > ul'))
-console.log(artList)
+// console.log(artList)
+
+
+
+let clicked = false;
 
 for (const btn of moreBtn) {
   btn.addEventListener('click', e => {
     artList.forEach(el => el.classList.toggle('showList'))
+    if(!clicked){
+      moreBtn.forEach(e=>e.innerHTML = 'zwiń')
+      clicked = true;
+    }else{
+      moreBtn.forEach(e=>e.innerHTML = 'czytaj więcej')
+    }
+   
   })
 }
