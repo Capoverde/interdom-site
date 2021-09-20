bbead268f399dd64110d7cd2eaa5e42b000c1feb
Piotr <piotr_as@yahoo.com>
2021-09-20 15:38:46 +0200

    menu button poprawiony

--- src/js/totopBtn.js
+++ src/js/totopBtn.js
@@ -6,18 +6,12 @@ window.addEventListener('scroll', e =>{
     }else{
         totop.classList.remove('showTotop')
     }
-
- 
 })
 
-totop.addEventListener('click',scrollToTop = e =>{
-    // e.preventDefault();
-    // document.body.scrollTop = 0; // Safari
-    // document.documentElement.scrollTop = 0; // chrome
 
-    var position =
-    document.body.scrollTop || document.documentElement.scrollTop;
-if (position) {
+totop.addEventListener('click',scrollToTop = e =>{
+let position = document.body.scrollTop || document.documentElement.scrollTop;
+   if (position) {
     window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
     scrollAnimation = setTimeout("scrollToTop()", 30);
 } else clearTimeout(scrollAnimation);
